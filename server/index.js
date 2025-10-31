7e2ebaec6362ab008c0a0efdbb11918b6438bb14
rajeshprivate007 <rajesh.sahu.private@gmail.com>
2025-10-31 20:56:13 +0530

    updated port in index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(helmet());
 
 // CORS configuration
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  origin: process.env.CLIENT_URL || 'http://localhost:4001',
   credentials: true
 }));
 
